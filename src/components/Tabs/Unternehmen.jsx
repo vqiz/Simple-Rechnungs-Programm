e3c289b992a7e36715f2dbc94d4f4151e75a8d9e
Dominic <vqizvqizvqiz@gmail.com>
2025-08-03 15:45:52 +0200

    Add avatar image and update Unternehmen tab UI
    Added a new avatar image to the public images directory. Updated the Unternehmen.jsx component to import additional MUI components and icons, and enhanced the InfoCard information text to clarify the importance of required fields for electronic invoicing standards.

--- src/components/Tabs/Unternehmen.jsx
+++ src/components/Tabs/Unternehmen.jsx
@@ -1,14 +1,15 @@
-import { Box, Typography } from '@mui/joy'
+import { Avatar, Box, FormControl, FormLabel, Input, Typography } from '@mui/joy'
 import React from 'react'
 import Headline from '../Headline'
 import InfoCard from '../InfoCard'
+import FactoryIcon from '@mui/icons-material/Factory';
+
 
 function Unternehmen() {
   return (
     <Box
       sx={{
         height: '100vh',
-
         overflowY: 'auto',
         display: 'flex',
         flexDirection: 'column',
@@ -19,11 +20,11 @@ function Unternehmen() {
     >
       <Headline>Unternehmensdaten Verwalten</Headline>
       <Box sx={{ p: 2 }}>
-        <InfoCard headline={"Information"}>Hier werden die Unternehmensdaten ihres <Typography sx={{ fontWeight: "bold" }}>eigenen</Typography> Unternehmens bearbeitet. Diese werden später auf Rechnungen bei <Typography sx={{ fontWeight: "bold" }}>Verkäufer</Typography> angezeigt</InfoCard>
+        <InfoCard headline={"Information"}>Hier werden die Unternehmensdaten ihres <Typography sx={{ fontWeight: "bold" }}>eigenen</Typography> Unternehmens bearbeitet. Diese werden später auf Rechnungen bei <Typography sx={{ fontWeight: "bold" }}>Verkäufer</Typography> angezeigt. <br />
+          Alle Pflichtfelder sind für eine E-Rechnung bzw. XRechnung nach geltendem Gesetzlichen Standart <Typography sx={{ fontWeight: "bold" }}>Unverzichtbar</Typography>! </InfoCard>
       </Box>
-
-
     </Box>
+ 
   )
 }
 
