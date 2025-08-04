93fcdea92620cadcb5c5be69e2282969eae05cca
Dominic <vqizvqizvqiz@gmail.com>
2025-08-04 18:53:44 +0200

    Enhance Unternehmen tab with contact and misc fields
    Added sections for Unternehmensdaten, Kontaktperson, and Sonstiges with new input fields for phone number, email, and contact details. Introduced dividers and a save button for improved UI structure and usability.

--- src/components/Tabs/Unternehmen.jsx
+++ src/components/Tabs/Unternehmen.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, FormControl, FormLabel, Input, Typography } from '@mui/joy'
+import { Avatar, Box, Button, Divider, FormControl, FormLabel, Input, Typography } from '@mui/joy'
 import React from 'react'
 import Headline from '../Headline'
 import InfoCard from '../InfoCard'
@@ -24,6 +24,9 @@ function Unternehmen() {
         <InfoCard headline={"Information"}>Hier werden die Unternehmensdaten ihres <Typography sx={{ fontWeight: "bold" }}>eigenen</Typography> Unternehmens bearbeitet. Diese werden später auf Rechnungen bei <Typography sx={{ fontWeight: "bold" }}>Verkäufer</Typography> angezeigt. <br />
           Alle Pflichtfelder sind für eine E-Rechnung bzw. XRechnung nach geltendem Gesetzlichen Standart <Typography sx={{ fontWeight: "bold" }}>Unverzichtbar</Typography>! </InfoCard>
       </Box>
+
+      <Typography sx={{ color: "gray", ml: 2 }} level="title-md">Unternehmensdaten</Typography>
+      <Divider orientation="horizontal"></Divider>
       <Box sx={{ display: "flex", flexDirection: "column", gap: 2, p: 2 }}>
         <Box sx={boxlinestyle}>
           <FormControl sx={{ width: "74.6%" }}>
@@ -40,36 +43,74 @@ function Unternehmen() {
             <FormLabel sx={labelstyle}>Straße {"(Pflichtfeld)"}</FormLabel>
             <Input placeholder='z.B. Musterstraße' />
           </FormControl>
-          <FormControl sx={{width: "23.5%"}}>
+          <FormControl sx={{ width: "23.5%" }}>
             <FormLabel sx={labelstyle}>Hausnummer {"(Pflichtfeld)"}</FormLabel>
-            <Input type='number' placeholder='z.B. 92' />
+            <Input placeholder='z.B. 92' />
           </FormControl>
           <FormControl>
             <FormLabel sx={labelstyle}>Stadt | Ort {"(Pflichtfeld)"}</FormLabel>
-            <Input placeholder='z.B. Musterstraße'/>
+            <Input placeholder='z.B. Straubing' />
           </FormControl>
         </Box>
         <Box sx={boxlinestyle}>
           <FormControl>
             <FormLabel sx={labelstyle}>Länder Code | ISO-Code {"(Pflichtfeld)"} </FormLabel>
-            <Input placeholder='z.B. DE'/>
+            <Input placeholder='z.B. DE' />
           </FormControl>
-          <FormControl sx={{width: "31%"}}>
+          <FormControl sx={{ width: "31%" }}>
             <FormLabel sx={labelstyle}>Umsatzsteuer-ID {"(Pflichtfeld)"}</FormLabel>
-            <Input placeholder='z.B. DE123456789'/>
+            <Input placeholder='z.B. DE123456789' />
           </FormControl>
-          <FormControl sx={{width: "39.5%"}}>
+          <FormControl sx={{ width: "39.5%" }}>
             <FormLabel sx={labelstyle}>Bankverbindung | für Sepa Lastschriften etc. {"(Pflichtfeld)"}</FormLabel>
             <Input placeholder='z.B. DE21 3704 0044 0532 0130 00' />
           </FormControl>
         </Box>
         <Box sx={boxlinestyle}>
-          <FormControl sx={{width: "50%"}}>
+          <FormControl sx={{ width: "50%" }}>
             <FormLabel sx={labelstyle}>Handelsregisternummer</FormLabel>
-            <Input/>
+            <Input />
+          </FormControl>
+          <FormControl sx={{ width: "39.5%" }}>
+            <FormLabel>Telefonnummer</FormLabel>
+            <Input placeholder='+49 1515 1145345' />
           </FormControl>
         </Box>
+        <Box sx={boxlinestyle}>
+          <FormControl sx={{ width: "50%" }}>
+            <FormLabel sx={labelstyle}>Emailadresse</FormLabel>
+            <Input placeholder='z.B. org.example@gmail.com' />
+          </FormControl>
+
+        </Box>
       </Box>
+      <Typography sx={{ color: "gray", ml: 2 }}>Kontaktperson</Typography>
+      <Divider orientation="horizontal" />
+      <Box sx={{ display: "flex", flexDirection: "column", gap: 2, p: 2 }}>
+        <Box sx={boxlinestyle}>
+          <FormControl sx={{ width: "50%" }}>
+            <FormLabel sx={labelstyle}>Name</FormLabel>
+            <Input placeholder='z.B Max Mustermann' />
+          </FormControl>
+          <FormControl sx={{ width: "39.5%" }}>
+            <FormControl sx={labelstyle}>Emailadresse</FormControl>
+            <Input placeholder='z.B max.musterman@t-online.de' />
+          </FormControl>
+        </Box>
+        <Box sx={boxlinestyle}>
+          <FormControl sx={{ width: "50%" }}>
+            <FormControl sx={labelstyle}>Telefonnummer</FormControl>
+            <Input placeholder='+49 1515 1145345' />
+          </FormControl>
+
+        </Box>
+      </Box>
+
+      <Typography sx={{ color: "gray", ml: 2 }}>Sonstiges</Typography>
+      <Divider orientation="horizontal" />
+      <Button sx={{ width: "10%", ml: 2 }}>
+        Speichern
+      </Button>
     </Box>
 
   )
