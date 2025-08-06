94749b5ada59d83ab178bba6ed5cdc6defd6608c
Dominic <vqizvqizvqiz@gmail.com>
2025-08-06 12:52:06 +0200

    Add stubs for KundenDatenBank, RechnungErstellen, Settings
    Created initial stub files for KundenDatenBank.js with a placeholder function, and added basic React component skeletons for RechnungErstellen and Settings. These serve as starting points for further development.

---
+++ src/Scripts/KundenDatenBank.js
@@ -0,0 +1,8 @@
+
+export const kundeErstellen = (name,istfirma,street,number,plz,ort,landcode, email,telefon,ansprechpartner, leitwegid) => {
+
+
+
+
+
+}
---
+++ src/components/Tabs/RechnungErstellen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RechnungErstellen() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RechnungErstellen
